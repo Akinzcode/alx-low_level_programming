ce4c21c270b8f3120bd2655bfd357ce543f7c787
Akinzcode <adegboyeolayinka2015@gmail.com>
2022-09-23 13:04:16 -0700

    lowlevel

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - concantenate two strings but add inputted number of bytes
  * @dest: string to be appended upon
@@ -12,6 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int index = 0, dest_len = o;
 
 	while (dest[index++])
+
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
